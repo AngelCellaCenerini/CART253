f7c0e00c03d29937cc9b1e1e9acddcee5ae2902f
Angel CC <angelcenerini@gmail.com>
2020-09-13 16:54:02 -0400

    Played with colors, fill, transparemcy

--- Experiments/Drawing Shapes Experiment/js/script.js
+++ Experiments/Drawing Shapes Experiment/js/script.js
@@ -11,7 +11,9 @@ Here is a description of this template p5 project.
 function setup() {
   createCanvas(500,500);
 background(200,200,200);
+fill(200,230,255, 200);
 ellipse(150,300,200,100);
+fill(30,255,169, 200); 
 ellipse(350,58,400,100);
 point(50,50);
 stroke(255,0,0);
