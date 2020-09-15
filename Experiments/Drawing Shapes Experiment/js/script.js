0ad47ade14489d36bef467e4c7efbd334f54d158
Angel CC <angelcenerini@gmail.com>
2020-09-15 16:32:09 -0400

    Activity 02: Started the draw an alien activity

--- Experiments/Drawing Shapes Experiment/js/script.js
+++ Experiments/Drawing Shapes Experiment/js/script.js
@@ -1,30 +1,35 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Trying out lessons from week 02
+Angel Cella Cenerini
 
-Here is a description of this template p5 project.
+Drawing experiments
 **************************************************/
 
 // setup()
 //
-// Description of setup() goes here.
+// Experimenting on shapes and colors.
 function setup() {
   createCanvas(500,500);
 background(200,200,200);
+
+// Light blue ellipse, standard lineart
 fill(200,230,255, 200);
 ellipse(150,300,200,100);
-fill(30,255,169, 200); 
-ellipse(350,58,400,100);
-point(50,50);
-stroke(255,0,0);
 
+fill(30,255,169, 200);
+ellipse(350,58,400,100);
+
+point(50,50);
+
+// acqua-green rectangle, with red lineart
+stroke(255,0,0);
 rect(24,68,40,80);
 
 }
 
 // draw()
 //
-// Description of draw() goes here.
+// Currently no Function Drawing
 function draw() {
 
 }
