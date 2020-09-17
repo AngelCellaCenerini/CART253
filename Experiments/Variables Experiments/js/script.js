2bdb64d2e52cfba1dca65f4ff2a705a949da9298
Angel CC <angelcenerini@gmail.com>
2020-09-17 16:58:00 -0400

    Created and set up Experimenting with Variables work

---
+++ Experiments/Variables Experiments/js/script.js
@@ -0,0 +1,20 @@
+/**************************************************
+Variables Experiments
+Angel Cella Cenerini
+
+Experimenting with week03 lessons
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
