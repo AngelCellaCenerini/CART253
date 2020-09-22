6cca53809c15403f94b3a1126d68f2e003a6cc55
Angel CC <angelcenerini@gmail.com>
2020-09-22 19:36:30 -0400

    E01: Started exercise 01

---
+++ Exercises/Exercise01/js/script.js
@@ -0,0 +1,20 @@
+/**************************************************
+Exercise 01 - Template p5 project by CART253 Course
+Angel Cella Cenerini
+
+Exercise 01 - I like to move it move it!
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
