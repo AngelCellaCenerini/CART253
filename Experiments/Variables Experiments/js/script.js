0850150d2811ef47bcdca4e091f24883fe4ada82
Angel CC <angelcenerini@gmail.com>
2020-09-22 17:21:38 -0400

    played around with built-in variables

--- Experiments/Variables Experiments/js/script.js
+++ Experiments/Variables Experiments/js/script.js
@@ -10,11 +10,19 @@ Experimenting with week03 lessons
 // Description of setup() goes here.
 function setup() {
 
+createCanvas(500,500);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+background(255,0,0);
+//playing with built-in Variables
+//ellipse(60,80,mouseY,mouseX);
+//fill(0);
+//ellipse(mouseX, mouseY, 100,100);
+
+
 
 }
