f14f5a330b637d7b985279263cece404991fd0e9
Angel CC <angelcenerini@gmail.com>
2020-09-29 22:00:54 -0400

    Started Experimenting with Conditionals

---
+++ Experiments/Conditionals Experiments/js/script.js
@@ -0,0 +1,20 @@
+/**************************************************
+Experimenting with Conditionals - template by CART253 course
+Angel Cella Cenerini
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
