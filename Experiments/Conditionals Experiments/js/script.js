c28821c7178a7710e770da00f4e0595dcaff18b6
Angel CC <angelcenerini@gmail.com>
2020-09-29 22:10:05 -0400

    Introduced/played with Conditionals

--- Experiments/Conditionals Experiments/js/script.js
+++ Experiments/Conditionals Experiments/js/script.js
@@ -4,17 +4,36 @@ Angel Cella Cenerini
 
 Here is a description of this template p5 project.
 **************************************************/
+//Declaring variables vua JS object
+let circle ={
+x: 0,
+y: 250,
+size: 140,
+speed: 2
+}
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-
+createCanvas(500,500);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
-
+  background(130);
+  push();
+  noStroke();
+  fill(240,30,289);
+  circle.x = circle.x + circle.speed;
+  if(circle.x>width/2){
+    circle.speed = -circle.speed;
+  }
+  else {
+    circle.speed= circle.speed;
+  }
+  ellipse(circle.x,circle.y,circle.size);
+  pop();
 }
