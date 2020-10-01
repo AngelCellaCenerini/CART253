3ba320ad6e5eacc0591915a23754b93f558987f0
Angel CC <angelcenerini@gmail.com>
2020-09-30 23:03:34 -0400

    E02: Introductory Description; setting Canvas and Default Background

--- Exercises/Exercise02/js/script.js
+++ Exercises/Exercise02/js/script.js
@@ -1,14 +1,24 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Exercise 02 - Template p5 project by CART253 Course
+Angel Cella Cenerini
 
-Here is a description of this template p5 project.
+Exercise 02: Dodge-em!
+
+BEST STUDENT!
+Try being the best student during COVID-19!
+Dodge Bad Internet Connection,
+      Procastination,
+      Staying Up Until 4:00AM and
+      Forgetting Assignments!
 **************************************************/
 
 // setup()
-//
+// Declaring (+assignment) customized variables as JavaScript objects - named after represented subject;
 // Description of setup() goes here.
+
 function setup() {
+  // Canvas occupies entirely space available on web page
+  createCanvas(windowWidth, windowHeight);
 
 }
 
@@ -16,5 +26,6 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
+  background(0);
 
 }
