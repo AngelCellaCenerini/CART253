cfd0566c1bdb6506b7da638dd3a60335dcf763be
Angel CC <angelcenerini@gmail.com>
2020-10-01 00:32:18 -0400

    E02: Setting objects' movements

--- Exercises/Exercise02/js/script.js
+++ Exercises/Exercise02/js/script.js
@@ -15,43 +15,43 @@ Dodge Bad Internet Connection,
 // Inserting images - .png files are objects user has to avoid
 // Bad Internet Connection (bic)
 let imgBic ={
-  x: 0,
-  y:250,
+  x: 200,
+  y:0,
   width: 100,
   height: 100,
   vx: 0,
   vy: 0,
-  speed: 5
+  speed: 1
 }
 // Forgetting Assignments (fa)
 let imgFa ={
   x: 100,
-  y: 350,
+  y: 0,
   width: 100,
   height: 100,
   vx: 0,
   vy: 0,
-  speed: 5
+  speed: 1
 }
 // Procastination (p)
 let imgP={
   x: 300,
-  y: 300,
+  y: 0,
   width: 100,
   height: 100,
   vx: 0,
   vy: 0,
-  speed: 5
+  speed: 1
 }
 // Staying up Until 4:00AM (su)
 let imgSu={
   x: 50,
-  y: 400,
+  y: 0,
   width: 100,
   height: 100,
   vx: 0,
   vy: 0,
-  speed: 5
+  speed: 1
 }
 
 
@@ -87,15 +87,27 @@ function draw() {
   background(0);
 
 // Bad Internet Connection(bic) icon
+imgBic.x = random(0,width);
+imgBic.y += imgBic.vy;
+imgBic.vy += imgBic.speed;
 image(bic,imgBic.x,imgBic.y,imgBic.width,imgBic.height);
 
 //Forgetting Assignments (fa) icon
+imgFa.x = random(0,width);
+imgFa.y += imgFa.vy;
+imgFa.vy += imgFa.speed;
 image(fa,imgFa.x,imgFa.y,imgFa.width,imgFa.height);
 
 //Procastination (p) icon
+imgP.x = random(0,width);
+imgP.y += imgP.vy;
+imgP.vy += imgP.speed;
 image(p,imgP.x,imgP.y,imgP.width,imgP.height);
 
 //Staying up Until 4AM (su) icon
+imgSu.x = random(0,width);
+imgSu.y += imgSu.vy;
+imgSu.vy += imgSu.speed;
 image(su,imgSu.x,imgSu.y,imgSu.width,imgSu.height);
 
 }
