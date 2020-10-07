31486e8554c7fae2f7d948fad93d4d789470b2f3
Angel CC <angelcenerini@gmail.com>
2020-10-07 17:27:19 -0400

    E03: Started exercise 03

---
+++ Exercises/Exercise03/js/script.js
@@ -0,0 +1,29 @@
+/**************************************************
+Exercise 03 - Template p5 project by CART253 Course
+Angel Cella Cenerini
+
+Exercise 03 - Looking for love
+
+COUNTING SHEEP(S)
+You have until dawn to play ;)
+Don't blink! It might be morning when you re-open your eyes
+
+**************************************************/
+
+// setup()
+// Declaring (+assignment) customized variables as JavaScript objects - named after represented subject
+
+
+function setup() {
+
+
+
+}
+
+// draw()
+//
+// Designing movable/interactive picture;
+function draw() {
+
+
+}
