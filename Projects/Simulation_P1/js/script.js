c4dd059349e6be4cc0722697dfe755f6eef43366
Angel CC <angelcenerini@gmail.com>
2020-10-12 21:46:10 -0400

    P1: Started Project01
    Inserted image and font files.
    Set up introductory comments

---
+++ Projects/Simulation_P1/js/script.js
@@ -0,0 +1,21 @@
+/**************************************************
+PROJECT 01 - Simulation
+Template by CART253 Course
+Angel Cella Cenerini
+
+CAT OWNER SIMULATOR
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
