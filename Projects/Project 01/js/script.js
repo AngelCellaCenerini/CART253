aacc4116f5ed90705df7d0d900884235f724a1c7
Angel CC <angelcenerini@gmail.com>
2020-10-12 20:52:11 -0400

    P01: Fail?

---
+++ Projects/Project 01/js/script.js
@@ -0,0 +1,21 @@
+/**************************************************
+PROJECT 01 - Simulation
+Template p5 project by CART253 course
+Angel Cella Cenerini
+
+CAT OWNER SIMULATOR
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
