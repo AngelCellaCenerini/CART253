d7705f5cc7875c315670d45c3523abba99860cc0
Angel CC <angelcenerini@gmail.com>
2020-10-12 21:13:13 -0400

    P01: No need to restart
    The issue had to do with using strict mode...will ask more about situation

--- Projects/Project01_fail/js/script.js
+++ Projects/Project01_fail/js/script.js
@@ -14,8 +14,8 @@ Don't blink! It might be morning when you re-open your eyes
 
 //Declaring materials (image, font, sound files).
 //Declaring Font
-// let myFontTitle;
-// let myFontBody;
+let myFontTitle;
+let myFontBody;
 
 //Declaring Background
 let bg = {
@@ -39,146 +39,146 @@ let imgIntro = {
 }
 
 //Title - black cat with background
-// let imgBlackCat = {
+let imgBlackCat = {
+  x: 0,
+  y: 0,
+}
+
+//Menu - cat choises
+// let imgCat1 = {
 //   x: 0,
 //   y: 0,
+//   size: 550
 // }
 
-// //Menu - cat choises
-// // let imgCat1 = {
-// //   x: 0,
-// //   y: 0,
-// //   size: 550
-// // }
-//
-// // let imgCat2 = {
-// //   x: 0,
-// //   y: 0,
-// //   size: 550
-// // }
-// //
-// // let imgCat3 = {
-// //   x: 0,
-// //   y: 0,
-// //   size: 550
-// // }
-// //
-// // let imgCat4 = {
-// //   x: 0,
-// //   y: 0,
-// //   size: 550
-// // }
-//
-// //Level01
-// //Cat
-// let imgCat = {
+// let imgCat2 = {
 //   x: 0,
 //   y: 0,
 //   size: 550
 // }
 //
-// //BadEnding02
-// // let cryingCat = {
-// //   x: 0,
-// //   y: 0,
-// //   size: 500
-// // }
-//
-// //Level02 - Doors
-// let imgOpenedDoor = {
+// let imgCat3 = {
 //   x: 0,
 //   y: 0,
-//   width: 400,
-//   height: 500
+//   size: 550
 // }
 //
-// let imgDoor1 = {
+// let imgCat4 = {
 //   x: 0,
 //   y: 0,
-//   width: 400,
-//   height: 500
+//   size: 550
 // }
-//
-// let imgDoor2 = {
+
+//Level01
+//Cat
+let imgCat = {
+  x: 0,
+  y: 0,
+  size: 550
+}
+
+//BadEnding02
+// let cryingCat = {
 //   x: 0,
 //   y: 0,
-//   width: 400,
-//   height: 500
+//   size: 500
 // }
-//
-// let imgDoor3 = {
-//   x: 0,
-//   y: 0,
-//   width: 400,
-//   height: 500
-// }
-//
-// //Level03
-// //Kittens
-// let imgKittens = {
-//   x: 0,
-//   y: 0,
-// }
-//
-// //Bad ending03 - cat hoarder
-// let imgHoarder1 = {
-//   x: 0,
-//   y: 0,
-// }
-//
-// let imgHoarder2 = {
-//   x: 0,
-//   y: 0,
-// }
-//
-// let imgHoarder3 = {
-//   x: 0,
-//   y: 0,
-// }
-//
-// let imgHoarder4 = {
-//   x: 0,
-//   y: 0,
-// }
-//
-// let imgHoarder5 = {
-//   x: 0,
-//   y: 0,
-// }
-//
-// //Level04
-// //Petshop
-// let imgPetShop = {
-//   x: 0,
-//   y: 0,
-// }
-//
-// //Simple Toy
-// let imgSimpleToy = {
-//   x: 0,
-//   y: 0,
-//   size: 300
-// }
-//
-// //Expensive Toy
-// let imgExpensiveToy = {
-//   x: 0,
-//   y: 0,
-//   size: 300
-// }
-//
-// //Bad Ending 04
-// let imgStreetEnding = {
-//   x: 0,
-//   y: 0,
-// }
-//
-// //Happy Ending
-//  let imgHappyEnding = {
-//    x: 0,
-//    y: 0
-//
-// }
+
+//Level02 - Doors
+let imgOpenedDoor = {
+  x: 0,
+  y: 0,
+  width: 400,
+  height: 500
+}
+
+let imgDoor1 = {
+  x: 0,
+  y: 0,
+  width: 400,
+  height: 500
+}
+
+let imgDoor2 = {
+  x: 0,
+  y: 0,
+  width: 400,
+  height: 500
+}
+
+let imgDoor3 = {
+  x: 0,
+  y: 0,
+  width: 400,
+  height: 500
+}
+
+//Level03
+//Kittens
+let imgKittens = {
+  x: 0,
+  y: 0,
+}
+
+//Bad ending03 - cat hoarder
+let imgHoarder1 = {
+  x: 0,
+  y: 0,
+}
+
+let imgHoarder2 = {
+  x: 0,
+  y: 0,
+}
+
+let imgHoarder3 = {
+  x: 0,
+  y: 0,
+}
+
+let imgHoarder4 = {
+  x: 0,
+  y: 0,
+}
+
+let imgHoarder5 = {
+  x: 0,
+  y: 0,
+}
+
+//Level04
+//Petshop
+let imgPetShop = {
+  x: 0,
+  y: 0,
+}
+
+//Simple Toy
+let imgSimpleToy = {
+  x: 0,
+  y: 0,
+  size: 300
+}
+
+//Expensive Toy
+let imgExpensiveToy = {
+  x: 0,
+  y: 0,
+  size: 300
+}
+
+//Bad Ending 04
+let imgStreetEnding = {
+  x: 0,
+  y: 0,
+}
+
+//Happy Ending
+ let imgHappyEnding = {
+   x: 0,
+   y: 0
+
+}
 
 //Declaring state
 // let state = `title`; // Title, Menu, Level01, Level02, level03, level04; all exept `Title` contain both a good and a bad ending.
