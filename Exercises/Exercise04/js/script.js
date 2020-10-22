c1f750157f7570e5cb8563113b912705512ce3fb
Angel CC <angelcenerini@gmail.com>
2020-10-22 03:09:59 -0400

    E04: Set Up "Bad Ending" State
    Set Up "Bad Ending" State - User not catching any Magikarp;
    Inserted moving/jittering icon;
    Set Up "Turn to title screen" command by pressing ESC.

--- Exercises/Exercise04/js/script.js
+++ Exercises/Exercise04/js/script.js
@@ -11,18 +11,33 @@ Catch that magilove!
 // Cutomized Fonts
 let myFont;
 
-// Background - Underwater scene
+// Default Black Background; Title
+let bgD = {
+  r: 0,
+  g: 0,
+  b: 0,
+}
+
+// White Background; Endings
+let bg = {
+  r: 255,
+  g: 255,
+  b: 255,
+}
+
+// Background - Underwater scene; Simulation
 let imgWaterBackground = {
   x: 0,
   y: 0
 }
 
+// Title Screen Icon - MagiLove
 let imgTitleMagikarp = {
   x: 0,
   y: 0
 }
 
-// Objects - Magikarps Icons
+// Objects - Magikarps Icons; Simulation
 let magikarpIcons = [];
 let displayIcons;
 
@@ -32,8 +47,17 @@ let imgPokeball = {
   y: 0
 }
 
+// Pokemon (dragon)Fly Icon - Yanmega; Bad Ending
+let imgYanmega = {
+  x: 600,
+  y: 200,
+  vx: 0,
+  vy: 0,
+  speed: 5
+}
+
 // Declaring state(s)
-let state = `title` // Title, Simulation, Happy Ending 01, Happy Ending 02, Bad Ending
+let state = `bad ending` // Title, Simulation, Happy Ending 01, Happy Ending 02, Bad Ending
 
 function preload(){
  myFont = loadFont('assets/AnonymousPro-Regular.otf');
@@ -41,6 +65,7 @@ function preload(){
   waterBackground = loadImage('assets/images/waterBackground.jpg');
   pokeball = loadImage('assets/images/pokeball.png');
   titleMagikarp = loadImage('assets/images/titleMagikarp.png');
+  yanmega = loadImage(`assets/images/yanmega.png`);
 
   // for (let i = 0; i < 8; i ++){
   //   magikarpIcons[i] = loadImage('assets/images/magikarp-${i}.png');
@@ -58,6 +83,7 @@ function setup() {
   textFont(myFont);
   textAlign(CENTER, CENTER);
   noStroke();
+
 
    // magikarpIcons[0] = displayIcons(magikarpIcons, random(0,width), random(0, height));
    // for (let i = 0; i < 8; i ++){
@@ -77,11 +103,14 @@ function draw() {
 
   if (state === `title`){ // Black background
 
+  // Default Black Background
+  background(bgD.r, bgD.g, bgD.b);
+
   // Display Title Icon: Heart with Magikarp
   displayMagiHeart();
   // Display (White) Title Text
   displayTitleText();
-  // Start Simulation Command
+  // Start Simulation Command - SPACEBAR
   keyPressed();
   }
 
@@ -92,6 +121,19 @@ function draw() {
     displayCursor();
     // Display Objects: Magikarp Icons
      // image(displayIcons, random(0,width), random(0, height));
+}
+
+if (state === `bad ending`){
+  // White Background
+  background(bg.r, bg.g, bg.b);
+
+  // Movements - Yanmega Icon
+  yanmegaMovements();
+  // Display Yanmega Icon
+  displayYanmega();
+  // Bad Ending White Text
+  badEndingText();
+
 }
 
 
@@ -132,10 +174,53 @@ function displayCursor(){
 }
 //
 
-// Start Simulation Command
+// Movements - Yanmega Icon
+function yanmegaMovements(){
+  // Movements - Yanmega Icon
+  let change = random(0,1);
+  if (change < 0,05){
+      imgYanmega.vx = random (-imgYanmega.speed,imgYanmega.speed);
+      imgYanmega.vy = random (-imgYanmega.speed,imgYanmega.speed);
+  }
+  imgYanmega.x = imgYanmega.x + imgYanmega.vx;
+  imgYanmega.y = imgYanmega.y + imgYanmega.vy;
+
+  // Constrain Icon to Canvas
+  imgYanmega.x = constrain(imgYanmega.x, 0, width);
+  imgYanmega.y = constrain(imgYanmega.y, 0, height);
+}
+//
+
+// Display Yanmega Icon
+function displayYanmega(){
+  image(yanmega, imgYanmega.x, imgYanmega.y);
+}
+//
+
+// Bad Ending White Text
+function badEndingText(){
+  push();
+  fill(0);
+  textSize(30);
+  text(`Well, what did you expect?
+  The time run out, and you didn't catch anything.
+
+  I hope you enjoy this emptiness.`, width/2, height/2);
+  textSize(17);
+  text(`Press ESC to return to the Title Screen.`, width/2, 5*height/6);
+  pop();
+}
+//
+
+// Keyboard Commands
 function keyPressed(){
+  // Start Simulation Command
   if ((state === `title`) && (keyCode === 32)){
     state = `simulation`;
+  }
+  // Return to Title Screen Command
+  else if ((state === `bad ending`) && (keyCode === 27)){
+    state = `title`;
   }
 }
 //
