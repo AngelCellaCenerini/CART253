3c19c91a663e68528a0bedb428305e8c146288cf
Angel CC <angelcenerini@gmail.com>
2020-10-22 00:35:48 -0400

    E04: Set Up Customized Cursor
    Uploaded Cursor Image;
    Set Up Cursor Image;
    
    * "use strict" commands only work with ";". Ask in Studio time

--- Exercises/Exercise04/js/script.js
+++ Exercises/Exercise04/js/script.js
@@ -1,4 +1,4 @@
-"use strict";
+"use strict;";
 
 /**************************************************
 Exercise 04 - The Age of Aquariums
@@ -7,12 +7,22 @@ Angel Cella Cenerini
 
 Catch that magilove!
 **************************************************/
+// Pokeball icon (AKA User/Cursor)
+let imgPokeball = {
+  x: 0,
+  y: 0
+}
+
+function preload(){
+  pokeball = loadImage('assets/images/pokeball.png');
+}
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noCursor();
   imageMode(CENTER);
   textAlign(CENTER, CENTER);
 
@@ -24,5 +34,9 @@ function setup() {
 // Description of draw() goes here.
 function draw() {
   background(0);
+
+  imgPokeball.x = mouseX;
+  imgPokeball.y = mouseY;
+  image(pokeball, imgPokeball.x, imgPokeball.y);
 
 }
