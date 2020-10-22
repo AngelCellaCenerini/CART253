0c824e4beb031e687e6e4ab0e948809b7fbbba1a
Angel CC <angelcenerini@gmail.com>
2020-10-22 00:12:59 -0400

    E04: Started Exercise 04
    Modified Program Description

---
+++ Exercises/Exercise04/js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**************************************************
+Exercise 04 - The Age of Aquariums
+Template p5 project by CART253 course
+Angel Cella Cenerini
+
+Catch that magilove!
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
