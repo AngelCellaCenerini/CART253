4c32eeb7ddc4acf242371c4176095d164bee1e30
Angel CC <angelcenerini@gmail.com>
2020-10-22 00:52:45 -0400

    E04: Set Up "Simulation" State (with Customized Background)
    Set Up States (currently working on "Simulation");
    Set Up Customized Background;
    Organizing in functions and Commenting will be implicitly included in next updates

--- Exercises/Exercise04/js/script.js
+++ Exercises/Exercise04/js/script.js
@@ -7,14 +7,26 @@ Angel Cella Cenerini
 
 Catch that magilove!
 **************************************************/
+
+// Background - Underwater scene
+let imgWaterBackground = {
+  x: 0,
+  y: 0
+}
+
 // Pokeball icon (AKA User/Cursor)
 let imgPokeball = {
   x: 0,
   y: 0
 }
 
+// Declaring state(s)
+let state = `simulation` // Title, Simulation, Happy Ending 01, Happy Ending 02, Bad Ending
+
 function preload(){
+  waterBackground = loadImage('assets/images/waterBackground.jpg');
   pokeball = loadImage('assets/images/pokeball.png');
+
 }
 
 // setup()
@@ -34,11 +46,24 @@ function setup() {
 // Description of draw() goes here.
 function draw() {
   background(0);
+
+if (state === `simulation`){
+  // Customized Background - Underwater scene
+  displayCustomizedBackground();
   // Customized Cursor : Pokeball Icon; follows mouse movements
-  displayCursor();
+  displayCursor();  
+}
+
 
 }
 
+// Customized Background - Underwater scene
+function displayCustomizedBackground(){
+  imgWaterBackground.x = width/2;
+  imgWaterBackground.y = height/2;
+  image(waterBackground, imgWaterBackground.x, imgWaterBackground.y, windowWidth, windowHeight);
+}
+//
 
 // Customized Cursor : Pokeball Icon; follows mouse movements
 function displayCursor(){
