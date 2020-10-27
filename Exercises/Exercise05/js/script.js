97b799546e682a2cedfad1c9410cfe5f232dff5d
Angel CC <angelcenerini@gmail.com>
2020-10-27 16:39:46 -0400

    E05: Set Up "Title" State
    Also set up all different states in draw()

--- Exercises/Exercise05/js/script.js
+++ Exercises/Exercise05/js/script.js
@@ -2,8 +2,8 @@
 
 /**************************************************
 Cooking Simulator (or Survival)
-Template p5 project by CART253 Course
 Angel Cella Cenerini
+p5 Template by CART253 Course
 
 The user has 15 seconds to cook a delicious, somewhat edible, or insanely deadly dish for the diners!
 **************************************************/
@@ -31,7 +31,8 @@ let imgRip = {
 }
 
 // Set Up State(s)
-let state = `title`; // Title, Instructions, Simulation, Good Ending, Bad Ending 01, Bad Ending 02
+let state = `title`; // Title (Instructions icluded), Simulation, Good Ending, Bad Ending 01, Bad Ending 02
+
 
 function preload(){
   myFont = loadFont('assets/AnonymousPro-Regular.otf');
@@ -47,6 +48,11 @@ function preload(){
 // Description of setup() goes here.
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  imageMode(CENTER);
+  textAlign(CENTER, CENTER);
+  textFont(myFont);
+  noStroke();
+  noCursor();
 
 }
 
@@ -56,4 +62,60 @@ function setup() {
 function draw() {
   background(0);
 
+// Title (Instructions included)
+if (state === `title`){
+   titleText();
 }
+
+// Simulation - "Catching" ingredients
+else if (state === `simulation`){
+
+}
+
+// Good Ending - Best Quality Ingredients
+else if (state === `goodEnding`){
+
+}
+
+// Bad Ending 01 - Edible Ingredients
+else if (state === `badEnding01`){
+
+}
+
+// Bad Ending 02 - Inedible Ingredients
+else if (state === `badEnding02`){
+
+}
+
+}
+
+
+
+
+
+//
+function titleText(){
+  push();
+  // White Text
+  fill(255);
+  // Title
+  textSize(70);
+  text( `SURVIVAL COOKING SIMULATOR`, width/2, height/3);
+  // Description
+  textSize(22);
+  text(`Living on your own can be hard, especially if you're the one cooking.
+
+  ...Or, if you're cooking for guests.
+
+  Because time is relative, you have 15 seconds to prepare a somewhat decent meal!
+  Choose the best ingredients for your diners! `,width/2, 3*height/5);
+  // Instructions
+  textSize(16);
+  text(`Press SPACEBAR to begin`, width/2, 7*height/8);
+  // Red Line (Crossing "Survival")
+  stroke(255, 0, 0);
+  strokeWeight(5);
+  line(width/6, height/3, 3*width/8, height/3);
+  pop();
+}
+//
