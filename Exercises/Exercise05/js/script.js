6e6b0b2e04f82a15ea7f53744c0858f545461d81
Angel CC <angelcenerini@gmail.com>
2020-10-27 16:08:15 -0400

    E05: Uploaded Custom Content
    .png files and .otf file

--- Exercises/Exercise05/js/script.js
+++ Exercises/Exercise05/js/script.js
@@ -7,6 +7,38 @@ Angel Cella Cenerini
 
 The user has 15 seconds to cook a delicious, somewhat edible, or insanely deadly dish for the diners!
 **************************************************/
+// Declaring Custom Font
+let myFont;
+
+// Declaring Icons (for Endings)
+// Stars - Good Ending
+let michelinStars;
+let imgStars = {
+  x: 0,
+  y: 0
+}
+// Toilet - Bad Ending 01
+let toilet;
+let imgToilet = {
+  x: 0,
+  y: 0
+}
+// Tombstone - BadEnding02
+let tombstone;
+let imgRip = {
+  x: 0,
+  y: 0
+}
+
+
+function preload(){
+  myFont = loadFont('assets/AnonymousPro-Regular.otf');
+  
+  michelinStars = loadImage('assets/images/michelinStars.png');
+  toilet = loadImage('assets/images/toilet.png');
+  tombstone = loadImage('assets/images/rip.png');
+}
+//
 
 // setup()
 //
