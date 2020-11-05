3b5d3312be32b4ae9217753b0dc60dce57dc2f30
Angel CC <angelcenerini@gmail.com>
2020-11-05 00:56:23 -0500

    P02P: Created Super Class "Light"
    Lights in general will gently float around the screen.
    Size, color and speed will slightly vary depending on the lights subclassese

---
+++ Projects/Project02/Prototype/js/Light.js
@@ -0,0 +1,28 @@
+class Light {
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+    this.size = undefined;
+    this.vx = 0;
+    this.vy = 0;
+    this.speed = undefined;
+  }
+
+  move(){
+    // Lights gentlry float around screen
+    this.y = this.y + this.vy;
+    this.x = this.x + this.vx;
+    let change = random(0, 1);
+    if (change < 0.02){
+      this.vx = random(-this.speed, this.speed);
+      this.vy = random(-this.speed, this.speed);
+    }
+    this.x = constrain( this.x, 0, width);
+    this.y = constrain( this.y, 0, height);
+  }
+
+  display(){
+  // Specific features will be inserted in subclasses
+  // ellipse(this.x, this.y, this.size);
+  }
+}
--- Projects/Project02/Prototype/js/script.js
+++ Projects/Project02/Prototype/js/script.js
@@ -9,7 +9,7 @@ User, guided by "script", plays sounds(corresponding to lights) and compose """m
 **************************************************/
 
 // Declaring States
-let state = `intro`; // Intro, Simulation
+let state = `simulation`; // Intro, Simulation
 // setup()
 //
 // Description of setup() goes here.
@@ -17,6 +17,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
   noCursor();
+  textAlign(CENTER, CENTER);
 }
 
 // draw()
@@ -35,18 +36,29 @@ function draw() {
 function textIntro() {
   push();
   fill(255);
-  textAlign(CENTER, CENTER);
   textSize(30);
   text(`Project 02: Prototype - Playing Melody`, width/2, height/2);
   textSize(20);
-  text(`Press the keys as instructed by the "script". Press ENTER to start`, width/2, 3*height/5);
+  text(`Press the keys as instructed by the "script" to create a "melody". Press ENTER to start.`, width/2, 3*height/5);
   pop();
 }
 //
 
+// Simulation
+function textSimulation(){
+  push();
+  fill(255);
+  textSize(18);
+  text(`Press ESC anytime to return to Title Screen.`, width/2, 8*height/9);
+  pop();
+}
+
 // P5 Events
 function keyPressed(){
-  if (state === `intro` && keyCode === 13){
+  if ((state === `intro`) && (keyCode === 13)){
     state = `simulation`;
+  }
+  else if ((state === `simulation`) && (keyCode === 27)){
+    state = `intro`;
   }
 }
