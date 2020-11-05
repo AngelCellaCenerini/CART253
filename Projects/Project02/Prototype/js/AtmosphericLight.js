43316e31dda7d386a9290ff3a5b42b34d44d4300
Angel CC <angelcenerini@gmail.com>
2020-11-05 01:06:26 -0500

    P02P: Created "Atmospheric Light" Subclass
    These items will be the smallest lights, floating around at the (relatively) greatest speed.

---
+++ Projects/Project02/Prototype/js/AtmosphericLight.js
@@ -0,0 +1,16 @@
+class AtmosphericLight extends Light {
+  constructor(x, y){
+  super(x, y);
+  this.size = 20;
+  this.speed = 1.8;
+  }
+
+  display(){
+    push();
+    fill(250);
+    ellipse(this.x, this.y, this.size)
+    fill(250, 250, 250, 180);
+    ellipse(this.x, this.y, this.size + this.size/4);
+    pop();
+  }
+}
