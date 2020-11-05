58f7ff03d5b9cdd80c91d1dd7c0726f9e8bdd8a3
Angel CC <angelcenerini@gmail.com>
2020-11-05 00:28:10 -0500

    P02P: Declared States, Set Up "Intro" State
    "Intro" State:
    White Title + Brief Explanation in black background.

--- Projects/Project02/Prototype/js/script.js
+++ Projects/Project02/Prototype/js/script.js
@@ -8,11 +8,15 @@ Prototype for Project 02: Testing "Playing Melody" Simulation
 User, guided by "script", plays sounds(corresponding to lights) and compose """melody"""
 **************************************************/
 
+// Declaring States
+let state = `intro`; // Intro, Simulation
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noStroke();
+  noCursor();
 }
 
 // draw()
@@ -20,4 +24,29 @@ function setup() {
 // Description of draw() goes here.
 function draw() {
   background(0);
+
+  if (state === `intro`) {
+    textIntro();
+  } else if (state === `simulation`) {
+  }
+}
+
+// intro
+function textIntro() {
+  push();
+  fill(255);
+  textAlign(CENTER, CENTER);
+  textSize(30);
+  text(`Project 02: Prototype - Playing Melody`, width/2, height/2);
+  textSize(20);
+  text(`Press the keys as instructed by the "script". Press ENTER to start`, width/2, 3*height/5);
+  pop();
+}
+//
+
+// P5 Events
+function keyPressed(){
+  if (state === `intro` && keyCode === 13){
+    state = `simulation`;
+  }
 }
