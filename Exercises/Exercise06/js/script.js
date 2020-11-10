8d65b48fc3de4ecf7d38435456b0664a107a8b19
Angel CC <angelcenerini@gmail.com>
2020-11-10 17:40:14 -0500

    E06: Started Exercise 06
    Usual Basic SetUp

---
+++ Exercises/Exercise06/js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**************************************************
+Exercise 06: "Make Some Noise!"
+Template p5 project by CART253 Course
+Angel Cella Cenerini
+
+User will guide displyed object(s) through the tone of their voice in order to avoid hitting the obstacles.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+createCanvas(windowWidth, windowHeight);
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+background(0);
+}
