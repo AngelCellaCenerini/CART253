ad5e5d24667f44bf26ef087e037a7a5a1b43c8c3
Angel CC <angelcenerini@gmail.com>
2020-11-10 19:17:58 -0500

    Setting Up Graphics pt1
    All elements are initially being drawn in the main script -before creating the specific object-, for the results to be immediately visible.
    
    Pt1: Drawing Objects guided by User

--- Exercises/Exercise06/js/script.js
+++ Exercises/Exercise06/js/script.js
@@ -20,4 +20,77 @@ createCanvas(windowWidth, windowHeight);
 // Description of draw() goes here.
 function draw() {
 background(0);
+
+// White Lines
+push();
+stroke(255);
+strokeWeight(15);
+line(width/3, 0, width/3, height);
+line(2*width/3, 0, 2*width/3, height);
+strokeWeight(8);
+line(8*width/25, 0, 8*width/25, height);
+line(17*width/25, 0, 17*width/25, height);
+pop();
+
+// Winged Creature
+
+// Wings
+// Left Wing
+push();
+noStroke();
+fill(189, 195, 248);
+triangle(width/2 - 60, height/2 + 10, width/2 - 35, height/2 - 30, width/2, height/2);
+pop();
+
+push();
+noStroke();
+fill(235, 217, 255);
+triangle(width/2 - 90, height/2 - 10, width/2 - 35, height/2 - 30, width/2, height/2);
+pop();
+
+push();
+noStroke();
+fill(255);
+triangle(width/2 - 108, height/2 - 30, width/2 - 35, height/2 - 30, width/2, height/2);
+pop();
+
+// Right Wing
+push();
+noStroke();
+fill(189, 195, 248);
+triangle(width/2 + 60, height/2 + 10, width/2 + 35, height/2 - 30, width/2, height/2);
+pop();
+
+push();
+noStroke();
+fill(235, 217, 255);
+triangle(width/2 + 90, height/2 - 10, width/2 + 35, height/2 - 30, width/2, height/2);
+pop();
+
+push();
+noStroke();
+fill(255);
+triangle(width/2 + 108, height/2 - 30, width/2 + 35, height/2 - 30, width/2, height/2);
+pop();
+
+// Body
+push();
+noStroke();
+fill(255);
+ellipse(width/2, height/2, 60);
+fill(0);
+ellipse(width/2, height/2 - 15, 15, 10);
+ellipse(width/2, height/2, 20, 10);
+fill(255);
+ellipse(width/2, height/2 + 4, 20, 10);
+fill(0);
+ellipse(width/2, height/2, 6);
+pop();
+
+
+
+// Deer Horns
+push();
+pop();
+
 }
