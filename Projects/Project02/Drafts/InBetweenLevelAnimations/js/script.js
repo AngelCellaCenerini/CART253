65bb46f43b84ae4e7f275d799d4853708c4bd517
Angel CC <angelcenerini@gmail.com>
2020-11-16 23:57:48 -0500

    P02DA: Starting working on Animations
    P02DA = Project02 Draft: In-Between-Levels Animations (sort of cutscenes)

---
+++ Projects/Project02/Drafts/InBetweenLevelAnimations/js/script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**************************************************
+Project02 Draft: In Between Levels Animation
+Angel Cella Cenerini
+Template p5 project by CART253 Course
+
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
