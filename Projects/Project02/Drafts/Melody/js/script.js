1f0f24891c083cb2a40c9f72130b3a4602458320
Angel CC <angelcenerini@gmail.com>
2020-11-16 23:49:19 -0500

    P02DM: Testing out notes
    Changing notes order form random to ordered

--- Projects/Project02/Drafts/Melody/js/script.js
+++ Projects/Project02/Drafts/Melody/js/script.js
@@ -9,13 +9,17 @@ Template p5 project by CART253 Course
 P02DM: Creating Melody for Final Project via p5.sound
 **************************************************/
 let synth;
-let notes = [`C#1`, `A1`, `Ab4`, `Bb4`, `Db4`];
+let notes = [`G2`, `A3`, `B3`, `C3`, `D3`, `E3`, `F#3`, `G3`];
+let currentNote = 0;
 let interval;
+
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(windowWidth, windowHeight);
+  userStartAudio();
   synth = new p5.PolySynth();
     for (let i = 0; i < synth.audiovoices.length; i++) {
       let voice = synth.audiovoices[i];
@@ -27,17 +31,30 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
+
+  background(0);
+
+
+}
+
+function playNextNote() {
+  // Chose the note at the current position
+  let note = notes[currentNote];
+  // Play it
+  synth.play(note, 0.2, 0, 0.4);
+  // Increase the current position and go back to 0 when we reach the end
+  currentNote = currentNote + 1;
+  if (currentNote === notes.length) {
+    currentNote = 0;
+  }
+}
+
+function mousePressed(){
   if (interval === undefined) {
-      interval = setInterval(playRandomNote, 500);
+      interval = setInterval(playNextNote, 500);
     }
     else {
       clearInterval(interval);
       interval = undefined;
     }
-}
-
-function playRandomNote(){
-  // Play Notes Randomly
-  let note = random(notes);
-  synth.play(note, 1, 1, 1);
-}
+  }
