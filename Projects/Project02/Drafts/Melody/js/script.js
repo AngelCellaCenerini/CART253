e3877f10eb31c41cab9ed975989098eace86c8a5
Angel CC <angelcenerini@gmail.com>
2020-11-16 21:41:58 -0500

    P02DM: Set Up Notes Played Randomly

--- Projects/Project02/Drafts/Melody/js/script.js
+++ Projects/Project02/Drafts/Melody/js/script.js
@@ -1,22 +1,43 @@
 "use strict";
 
 /**************************************************
-Template p5 project
-Pippin Barr
+Project02 Draft: Melody
+Angel Cella Cenerini
+Template p5 project by CART253 Course
 
-Here is a description of this template p5 project.
+
+P02DM: Creating Melody for Final Project via p5.sound
 **************************************************/
+let synth;
+let notes = [`C#1`, `A1`, `Ab4`, `Bb4`, `Db4`];
+let interval;
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-
+  synth = new p5.PolySynth();
+    for (let i = 0; i < synth.audiovoices.length; i++) {
+      let voice = synth.audiovoices[i];
+      voice.oscillator.setType(`triangle`);
+    }
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  if (interval === undefined) {
+      interval = setInterval(playRandomNote, 500);
+    }
+    else {
+      clearInterval(interval);
+      interval = undefined;
+    }
+}
 
+function playRandomNote(){
+  // Play Notes Randomly
+  let note = random(notes);
+  synth.play(note, 1, 1, 1);
 }
