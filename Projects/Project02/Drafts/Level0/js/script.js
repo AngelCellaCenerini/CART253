661b8538c0d0b67b722bdbb80933e60bfc5969a2
Angel CC <angelcenerini@gmail.com>
2020-11-17 21:56:40 -0500

    P02DL0: Started working on Level0
    Project02 Draft: Level0

---
+++ Projects/Project02/Drafts/Level0/js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**************************************************
+Project02 Draft: Level0
+Template p5 project by CART 253
+
+User must
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+createCanvas(windowWidth, windowHeight);
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
