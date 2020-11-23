64915a784de7e3178ecc5221cccd3d507c4fa12c
Angel CC <angelcenerini@gmail.com>
2020-11-23 16:59:42 -0500

    P02DL02: Started Level02 Draft
    Project 02 - Drafts: Level02

---
+++ Projects/Project02/Drafts/Level02/js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**************************************************
+Project02: Level 02 Draft
+Template p5 project by CART253 Course
+
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+  background(0);
+
+}
