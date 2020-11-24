bfe62fd090a51452c924f8e2bf75e7c4960811f2
Angel CC <angelcenerini@gmail.com>
2020-11-23 21:18:34 -0500

    P02DL02: Added movement to Moon super class

--- Projects/Project02/Drafts/Level02/js/Moon.js
+++ Projects/Project02/Drafts/Level02/js/Moon.js
@@ -7,12 +7,36 @@ class Moon{
     this.positionX = positionX;   // seperate coordinates: moons remain still, only reflections move
     this.positionY = positionY;   // seperate coordinates: moons remain still, only reflections move
     this.radius = 5;
+    this.vx = 3;
+    this.vy = 0;
+    this.speed = 3;
     this.red = undefined;
     this.green = undefined;
     this.blue = undefined;
     this.r = undefined;
     this.g = undefined;
     this.b = undefined;
+
+  }
+
+  move(){
+
+    this.positionX -= this.vx;
+
+    // Differentiate singual movements
+    let r = random(0, 1);
+    if (r < 0.05){
+      this.vx = random(-this.speed, this.speed);
+      this.vy = random(-this.speed, this.speed);
+    }
+
+    // Restrain movement
+    if (this.positionX > (this.x + this.size/6)){
+       this.vx = -this.vx;
+    }
+    else if(this.positionX < (this.x - this.size/6)){
+      this.vx = -this.vx;
+    }
 
   }
 
@@ -38,7 +62,8 @@ class Moon{
     push();
     fill(255, 253, 243);
     rect(this.positionX, this.positionY + 7*this.size/9, 2*this.size/3, this.size/30, this.radius, this.radius, this.radius, this.radius);
-    rect(this.positionX + this.size/8, this.positionY + 8*this.size/9, 2*this.size/9, this.size/30, this.radius, this.radius, this.radius, this.radius);
+    rect(this.positionX + this.size/8, this.positionY + 8*this.size/9, this.size/4, this.size/30, this.radius, this.radius, this.radius, this.radius);
+    rect(this.positionX + this.size/5, this.positionY + 8*this.size/9, this.size/5, this.size/30, this.radius, this.radius, this.radius, this.radius);
     rect(this.positionX, this.positionY + 19*this.size/18, this.size/3, this.size/30, this.radius, this.radius, this.radius, this.radius);
     pop();
 
--- Projects/Project02/Drafts/Level02/js/script.js
+++ Projects/Project02/Drafts/Level02/js/script.js
@@ -43,30 +43,8 @@ function draw() {
   for(let i = 0; i < moons.length; i++){
     let moon = moons[i];
     moon.display();
+    moon.move();
   }
 
-
-  // // Moon
-  // push();
-  // fill(246, 73, 76);
-  // ellipse(0, height/3, 450);
-  // pop();
-  //
-  // // Moon Spots
-  // push();
-  // fill(206, 53, 56);
-  // ellipse(112, height/3, 50);
-  // ellipse(56, height/3, 25);
-  // ellipse(90, height/3 - 112, 15);
-  // ellipse(50, height/3 + 112, 75, 60);
-  // pop();
-  //
-  // // Water Reflection
-  // push();
-  // fill(255, 253, 243);
-  // rect(0, height/3 + 350, 300, 15, 5, 5, 5, 5);
-  // rect(0 + 56, height/3 + 400, 100, 15, 5, 5, 5, 5);
-  // rect(0, height/3 + 475, 150, 15, 5, 5, 5, 5);
-  // pop();
 
 }
