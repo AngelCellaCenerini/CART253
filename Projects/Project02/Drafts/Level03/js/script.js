ddae446e3c5ea348b4cb505df2f8be7c7735b940
Angel CC <angelcenerini@gmail.com>
2020-11-24 23:57:29 -0500

    P02DL03: Created/Inserted Frog Object

--- Projects/Project02/Drafts/Level03/js/script.js
+++ Projects/Project02/Drafts/Level03/js/script.js
@@ -5,6 +5,7 @@ Template p5 project by CART253 course
 
 Here is a description of this template p5 project.
 **************************************************/
+let frog;
 
 // setup()
 //
@@ -13,6 +14,12 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
   noStroke();
+
+  let x = width/2;
+  let y = 3*height/4;
+  let positionX = width/2;
+  let positionY = 3*height/4;
+  frog = new Frog(x, y, positionX, positionY);
 }
 
 // draw()
@@ -21,46 +28,51 @@ function setup() {
 function draw() {
   background(0);
 
-  // Frog Chin
-  push();
-  fill(254, 254, 191);
-  rect(width/2 + 26, 3*height/4 + 42, 140, 130, 5, 5, 5, 5);
-  pop();
-
-  // Frog Body
-  push();
-  translate(width/2, 3*height/4);
-  rotate(PI / 4.0);
-  fill(79, 124, 247);
-  rect(0, 0, 120, 190, 5, 5, 5, 5);
-  pop();
-
-  // Frog Leg
-  push();
-  fill(70, 104, 238);
-  triangle(width/2 - 117, 3*height/4 + 108, width/2 - 182, 3*height/4 - 105, width/2 + 13, 3*height/4 + 108 );
-  pop();
-
-  // Frog Eye
-  push();
-  fill(173, 170, 255);
-  ellipse(width/2 + 32, 3*height/4 - 30, 40);
-  fill(0);
-  rect(width/2 + 32, 3*height/4 - 30, 20, 5);
-  pop();
-
-  // Frog Cheek
-  push();
-  fill(255, 153, 51);
-  ellipse(width/2 + 26, 3*height/4 + 17.5, 50);
-  pop();
+  frog.display();
+  frog.grow();
+  //
+  // // Frog Chin
+  // push();
+  // fill(254, 254, 191);
+  // rect(width/2 + 26, 3*height/4 + 42, 130, 130, 5, 5, 5, 5);
+  // pop();
+  //
+  // // Frog Body
+  // push();
+  // translate(width/2, 3*height/4);
+  // rotate(PI / 4.0);
+  // fill(79, 124, 247);
+  // rect(0, 0, 130, 210, 5, 5, 5, 5);
+  // pop();
+  //
+  // // Frog Leg
+  // push();
+  // fill(70, 104, 238);
+  // triangle(width/2 - 130, 3*height/4 + 105, width/2 - 148, 3*height/4 - 21, width/2 + 20, 3*height/4 + 105 );
+  // fill(0);
+  // rect(width/2, 3*height/4 + 115, 200, 20);
+  // pop();
+  //
+  // // Frog Eye
+  // push();
+  // fill(173, 170, 255);
+  // ellipse(width/2 + 32, 3*height/4 - 52, 40);
+  // fill(0);
+  // rect(width/2 + 32, 3*height/4 - 52, 25, 5);
+  // pop();
+  //
+  // // Frog Cheek
+  // push();
+  // fill(255, 153, 51);
+  // ellipse(width/2 , 3*height/4, 55);
+  // pop();
 
   // Hole
   // push();
   // fill(255, 135, 117);
-  // ellipse(width/2 + 26, 3*height/4 + 21, 50);
+  // ellipse(width/2 + 26, 3*height/4 + 2, 55);
   // fill(0);
-  // ellipse(width/2 + 26, 3*height/4 + 17.5, 50);
+  // ellipse(width/2 + 26, 3*height/4, 55);
   // pop();
 
 }
