2cc49831289b793b998ec8bd644f82d1a09ada93
Angel CC <angelcenerini@gmail.com>
2020-11-24 20:26:50 -0500

    P02DL03: Started Level03 for Final Project
    Project 02 - Drafts: Level03

---
+++ Projects/Project02/Drafts/Level03/js/script.js
@@ -0,0 +1,21 @@
+/**************************************************
+Project02 - Draft: Level03
+Angel Cella Cenerini
+Template p5 project by CART253 course
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
