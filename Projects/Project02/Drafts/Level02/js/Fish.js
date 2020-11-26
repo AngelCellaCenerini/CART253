fd7fbde5816f5a2fa7bc4f630c6b066f8ab48ac9
Angel CC <angelcenerini@gmail.com>
2020-11-26 17:13:14 -0500

    P02DL03: Studio Session
    (Prof.) Pippin's assistance:
    - fixed Wave coordination with mouse;
    - fixed Fish class rotation;
    - adjusted "chasing" mechanics;

--- Projects/Project02/Drafts/Level02/js/Fish.js
+++ Projects/Project02/Drafts/Level02/js/Fish.js
@@ -1,136 +1,130 @@
 class Fish{
-  constructor(x, y){
+  constructor(x, y, moon, wave, target){
     this.x = x;
     this.y = y;
+    this.chaseX = x;
+    this.chaseY = y;
+    this.moon = moon;
+    this.wave = wave;
+    this.target = moon;
     this.width = 60;
     this.height = 110;
     this.vx = 3;
     this.vy = 3;
     this.speed = 3;
     this.eatingTime = 0;
+    this.angle = 0;
+    this.active = true;
   }
 
   rotate(){
-    let angle = 0;
-    rotate(angle);
-    angle = angle + 0.05;
+    this.angle = this.angle + 0.01;
   }
 
-  // move(){
-  //   // Choose whether to change direction
-  //   let change = random(0, 1);
-  //   if (change < 0.02) {
-  //    this.vx = random(-this.speed, this.speed);
-  //    this.vy = random(-this.speed, this.speed);
-  //   }
-  //
-  //   // Move the fish
-  //   this.x = this.x + this.vx;
-  //   this.y = this.y + this.vy;
-  // }
+  chase(){
 
-  // chase(moon){
-  //
-  //   this.x = this.x + this.vx;
-  //   this.y = this.y + this.vy;
-  //
-  //   let dx = this.x - (moon.x + moon.size/2);
-  //   let dy = this.y - (moon.y + moon.size/2);
-  //
-  //   if (dx < 0){
-  //     this.vx = this.speed;
-  //   }
-  //   else if(dx > 0){
-  //     this.vx = -this.speed;
-  //   }
-  //
-  //   if (dy < 0){
-  //     this.vy = this.speed;
-  //   }
-  //   else if(dy > 0){
-  //     this.vy = -this.speed;
-  //   }
-  // }
-  //
-  // eat(moon){
-  //
-  //   let d = dist(this.x,this.y, moon.positionX, moon.positionY);
-  //
-  //   if ((moon.active) && ((d < this.width/2 + moon.size/2) || (d < this.height/2 + moon.size/2))){
-  //     this.eatingTime = 0;
-  //   }
-  //
-  //   this.eatingTime++;
-  //
-  //   if (this.eatingTime > 5*60){
-  //       moon.active = false;
-  //   }
-  //
-  // }
-
-  react(){
-    if (mouseIsPressed){ //others don't wok
-
-      let distX = this.x - mouseX;
-      let distY = this.y - mouseY;
-
-      if (distX < 0){
-        this.vx = this.speed;
-      }
-      else if(distX > 0){
-        this.vx = -this.speed;
-      }
-
-      if (distY < 0){
-        this.vy = this.speed;
-      }
-      else if(distY > 0){
-        this.vy = -this.speed;
-      }
-      setTimeout(this.hunt, 6000);
-    }
+    if(!this.target.active){
+      this.target = this.moon;
     }
 
+    let dx = this.x - (this.target.chaseX);
+    let dy = this.y - (this.target.chaseY);
 
+    if (dx < 0){
+      this.vx = this.speed;
+    }
+    else if(dx > 0){
+      this.vx = -this.speed;
+    }
+
+    if (dy < 0){
+      this.vy = this.speed;
+    }
+    else if(dy > 0){
+      this.vy = -this.speed;
+    }
+
+    this.x = this.x + this.vx;
+    this.y = this.y + this.vy;
+
+    this.chaseX = this.x;
+    this.chaseY = this.y;
+  }
+
+
+
+  eat(){
+
+    let d = dist(this.x,this.y, this.target.chaseX, this.target.chaseY);
+
+    if ((this.target.active) && (d < this.width/2 + 50)){
+        this.eatingTime++;
+    }
+    else{
+      this.eatingTime = 0;
+    }
+
+    if (this.eatingTime > 3*60){
+        this.target.active = false;
+        this.eatingTime = 0;
+    }
+
+  }
+
+  isClicked(){
+    let d = dist(this.x, this.y, mouseX, mouseY);
+    if(d < this.width){
+       return true;
+    }
+    else{
+      return false;
+    }
+  }
 
   display(){
 
-    let angle = 0;
-    rotate(angle);
-    // Fish Fins and Tail
-    push();
-    fill(246, 73, 76);
-    triangle(this.x - this.width, this.y + this.height/11, this.x + this.width, this.y + this.width/11, this.x, this.y + 4*this.height/11);
-    fill(2, 179, 237);
-    triangle(this.x - this.width/2, this.y - 12*this.height/11, this.x + this.width/2, this.y - 12*this.height/11, this.x, this.y - 8*this.height/11);
-    fill(0);
-    stroke(0);
-    triangle(this.x - 7*this.width/15, this.y - 12*this.height/11, this.x + 7*this.width/15, this.y - 12*this.height/11, this.x, this.y - 10*this.height/11);
-    angle = angle + 0.05;
-    pop();
+    if(this.active){
+      // Fish Fins and Tail
+      push();
+      translate(this.x, this.y);
+      rotate(this.angle);
+      fill(246, 73, 76);
+      triangle(0 - this.width, 0 + this.height/11, 0 + this.width, 0 + this.width/11, 0, 0 + 4*this.height/11);
+      fill(2, 179, 237);
+      triangle(0 - this.width/2, 0 - 12*this.height/11, 0 + this.width/2, 0 - 12*this.height/11, 0, 0 - 8*this.height/11);
+      fill(0);
+      stroke(0);
+      triangle(0 - 7*this.width/15, 0 - 12*this.height/11, 0 + 7*this.width/15, 0 - 12*this.height/11, 0, 0 - 10*this.height/11);
+      pop();
 
-    // Fish Body
-    push();
-    fill(255);
-    ellipse(this.x, this.y, this.width, this.height);
-    fill(0);
-    rect(this.x, this.y, this.width/4, 13*this.height/12);
-    pop();
+      // Fish Body
+      push();
+      translate(this.x, this.y);
+      rotate(this.angle);
+      fill(255);
+      ellipse(0, 0, this.width, this.height);
+      fill(0);
+      rect(0, 0, this.width/4, 13*this.height/12);
+      pop();
 
-    // Fish Spine
-    push();
-    fill(255);
-    rect(this.x, this.y, this.width/30, 5*this.height/4);
-    pop();
+      // Fish Spine
+      push();
+      translate(this.x, this.y);
+      rotate(this.angle);
+      fill(255);
+      rect(0, 0, this.width/30, 5*this.height/4);
+      pop();
 
-    // Fish Head
-    push();
-    stroke(255);
-    strokeWeight(2);
-    fill(0);
-    arc(this.x, this.y + 6*this.height/11, 2*this.width/3,  6*this.height/11, 0, PI , CHORD);
-    pop();
+      // Fish Head
+      push();
+      translate(this.x, this.y);
+      rotate(this.angle);
+      stroke(255);
+      strokeWeight(2);
+      fill(0);
+      arc(0, 0 + 6*this.height/11, 2*this.width/3,  6*this.height/11, 0, PI , CHORD);
+      pop();
 
-
+    }
   }
 }
--- Projects/Project02/Drafts/Level02/js/Wave.js
+++ Projects/Project02/Drafts/Level02/js/Wave.js
@@ -2,6 +2,8 @@ class Wave{
   constructor(x, y){
    this.x = x;
    this.y = y;
+   this.chaseX = x;
+   this.chaseY = y;
    this.size = 50;
    this.originalSize = 50;
    this.growth = 30;
@@ -10,13 +12,20 @@ class Wave{
 
   }
 
-  appear(note){
-    if(mouseIsPressed){
+
+  mousePressed(){
       if(!this.active){
         this.active = true;
+        this.x = mouseX;
+        this.y = mouseY;
+        this.chaseX = this.x;
+        this.chaseY = this.y;
+        return true;
+      }
+      else{
+        return false;
       }
     }
-  }
 
   grow(){
 
