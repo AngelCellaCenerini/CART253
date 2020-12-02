d0e9199f478628e88e79d23e5cb61af92c091c95
Angel CC <angelcenerini@gmail.com>
2020-12-02 00:03:55 -0500

    P02DL04: Created Arrow Class
    - Temporarily drew Arrow in setup();
    - Created Class (currently only display() method);
    - Inserted Arrow main script.

---
+++ Projects/Project02/Drafts/Level04/js/Arrow.js
@@ -0,0 +1,37 @@
+class Arrow{
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+    this.width = 3;
+    this.height = 150;
+  }
+
+  display(){
+    // Arrow
+    // Body
+    push();
+    fill(255);
+    rect(this.x, this.y, this.width, this.height);
+    pop();
+
+    // Tip
+    push();
+    stroke(255);
+    strokeWeight(3);
+    line(this.x - 5*this.width, this.y - this.height/3, this.x, this.y - this.height/2);
+    line(this.x + 5*this.width, this.y - this.height/3, this.x, this.y - this.height/2);
+    line(this.x - 5*this.width, this.y - this.height/3, this.x, this.y + this.height/6);
+    line(this.x + 5*this.width, this.y - this.height/3, this.x, this.y + this.height/6);
+    pop();
+
+    // Tail
+    push();
+    stroke(255);
+    strokeWeight(3);
+    line(this.x - 5*this.width, this.y + 19*this.height/30, this.x, this.y + 7*this.height/15);
+    line(this.x + 5*this.width, this.y + 19*this.height/30, this.x, this.y + 7*this.height/15);
+    line(this.x - 5*this.width, this.y + 4*this.height/5, this.x, this.y + 19*this.height/30);
+    line(this.x + 5*this.width, this.y + 4*this.height/5, this.x, this.y + 19*this.height/30);
+    pop();
+  }
+}
--- Projects/Project02/Drafts/Level04/js/script.js
+++ Projects/Project02/Drafts/Level04/js/script.js
@@ -5,12 +5,24 @@ Angel Cella Cenerini
 Template by CART253 course
 Level04
 **************************************************/
+let arrows = [];
+let numArrows = 7;
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noStroke();
+  rectMode(CENTER);
+
+  // Arrow(s)
+  for (let i = 0; i < numArrows; i++){
+    let x = random(0, width);
+    let y = random(0, height);
+    let arrow = new Arrow(x, y);
+    arrows.push(arrow);
+  }
 
 }
 
@@ -19,4 +31,38 @@ function setup() {
 // Description of draw() goes here.
 function draw() {
 background(0);
+
+for (let i = 0; i < arrows.length; i++){
+  let arrow = arrows[i];
+  arrow.display();
+}
+
+// // Arrow
+// // Body
+// push();
+// fill(255);
+// rect(width/2, height/2, 3, 150);
+// pop();
+//
+// // Tip
+// push();
+// stroke(255);
+// strokeWeight(3);
+// line(width/2 - 15, height/2 - 50, width/2, height/2 - 75);
+// line(width/2 + 15, height/2 - 50, width/2, height/2 - 75);
+// line(width/2 - 15, height/2 - 50, width/2, height/2 + 25);
+// line(width/2 + 15, height/2 - 50, width/2, height/2 + 25);
+// pop();
+//
+// // Tail
+// push();
+// stroke(255);
+// strokeWeight(3);
+// line(width/2 - 15, height/2 + 95, width/2, height/2 + 70);
+// line(width/2 + 15, height/2 + 95, width/2, height/2 + 70);
+// line(width/2 - 15, height/2 + 120, width/2, height/2 + 95);
+// line(width/2 + 15, height/2 + 120, width/2, height/2 + 95);
+// pop();
+
+
 }
