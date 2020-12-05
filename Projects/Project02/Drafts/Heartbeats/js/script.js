e14308493175adcfdd6a4e72a73e0c081fd3f129
Angel CC <angelcenerini@gmail.com>
2020-12-05 17:42:08 -0500

    P02DH: Started working on Project02 - Drafts: Heartbeats (sound effects intended for final project)
    Basic SetUp

---
+++ Projects/Project02/Drafts/Heartbeats/js/script.js
@@ -0,0 +1,22 @@
+/**************************************************
+Template p5 by CART253 Course
+Angel Cella Cenerini
+
+Project02 Drafts - Heartbeats (Sound effects intended for Final Project)
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+  background(0);
+
+}
