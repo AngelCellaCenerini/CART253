16568429e764d6220d2477178270d85363bd3594
Angel CC <angelcenerini@gmail.com>
2020-12-06 00:18:56 -0500

    P02DI: Started working on Project02 - Drafts: Intro (Title Screen + Instructions)
    - Basic SetUp

---
+++ Projects/Project02/Drafts/Intro/js/script.js
@@ -0,0 +1,22 @@
+/**************************************************
+Template p5 project by CART253 Course
+ANgel Cella Cenerini
+
+Project 02 - Drafts: Intro (Title Screen + Instructions)
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+  background(0);
+
+}
