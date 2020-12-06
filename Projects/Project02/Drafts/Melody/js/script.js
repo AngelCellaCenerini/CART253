1bfb0f8c60ed7f6230ee8ccc2373c97b8a5d67af
Angel CC <angelcenerini@gmail.com>
2020-12-06 15:02:35 -0500

    P02DM: Temporarily created 2 Voices
    - Final melody/motes yet to be inserted.
    - might make Meldoy into OOP

--- Projects/Project02/Drafts/Melody/js/script.js
+++ Projects/Project02/Drafts/Melody/js/script.js
@@ -9,9 +9,13 @@ Template p5 project by CART253 Course
 P02DM: Creating Melody for Final Project via p5.sound
 **************************************************/
 let synth;
-let notes = [`G2`, `A3`, `B3`, `C3`, `D3`, `E3`, `F#3`, `G3`];
+let synth2;
+let notes = [`F3`, `E3`, `D3`];
+let notes2 = [`A2`, `A#2`, `Ab2`];
 let currentNote = 0;
+let currentNote2 = 0;
 let interval;
+let interval2;
 
 
 // setup()
@@ -25,6 +29,12 @@ function setup() {
       let voice = synth.audiovoices[i];
       voice.oscillator.setType(`triangle`);
     }
+
+    synth2 = new p5.PolySynth();
+      for (let i = 0; i < synth2.audiovoices.length; i++) {
+        let voice2 = synth2.audiovoices[i];
+        voice2.oscillator.setType(`triangle`);
+      }
 }
 
 // draw()
@@ -41,11 +51,25 @@ function playNextNote() {
   // Chose the note at the current position
   let note = notes[currentNote];
   // Play it
-  synth.play(note, 0.2, 0, 0.4);
+  synth.play(note, 0.6, 0, 0.4);
   // Increase the current position and go back to 0 when we reach the end
   currentNote = currentNote + 1;
   if (currentNote === notes.length) {
     currentNote = 0;
+  }
+
+}
+
+function playNextNote2() {
+  // Chose the note at the current positionNote];
+  let note2 = notes2[currentNote2];
+  // Play it;
+  synth2.play(note2, 0.6, 0, 0.4);
+  // Increase the current position and go back to 0 when we reach the end
+
+  currentNote2 = currentNote2 + 1;
+  if (currentNote2 === notes2.length) {
+    currentNote2 = 0;
   }
 }
 
@@ -58,3 +82,13 @@ function mousePressed(){
       interval = undefined;
     }
   }
+
+  function keyPressed(){
+    if (interval2 === undefined) {
+        interval2 = setInterval(playNextNote2, 500);
+      }
+      else {
+        clearInterval(interval2);
+        interval2 = undefined;
+      }
+    }
