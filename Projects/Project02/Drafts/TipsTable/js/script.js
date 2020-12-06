a3e022b8f87ec4a09cdb91798121abb8430b691d
Angel CC <angelcenerini@gmail.com>
2020-12-06 18:56:56 -0500

    P02DTT: Started Working on TipsTable OOP for Final Project
    Basic SetUp

---
+++ Projects/Project02/Drafts/TipsTable/js/script.js
@@ -0,0 +1,20 @@
+/**************************************************
+Template p5 by CART253
+Angel Cella Cenerini
+
+Project 02 - Drafts: TipsTable OOP
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+createCanvas(windowWidth, windowHeight);
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+background(0);
+}
