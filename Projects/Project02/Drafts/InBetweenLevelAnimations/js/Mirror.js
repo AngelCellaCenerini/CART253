d6167cf7e21e356b090f3820ea4c1d868e8df02a
Angel CC <angelcenerini@gmail.com>
2020-12-05 22:43:18 -0500

    P02DA: Adjusted Mirror Class/methods
    Also removed PinkLight Subclass

--- Projects/Project02/Drafts/InBetweenLevelAnimations/js/Mirror.js
+++ Projects/Project02/Drafts/InBetweenLevelAnimations/js/Mirror.js
@@ -1,57 +1,43 @@
 class Mirror{
-  constructor(x, y, handleY, shadowX, shadowY){
+  constructor(x, y){
     this.x = x;
     this.y = y;
-    this.handleY = handleY;
-    this.shadowX = shadowX;
-    this.shadowY = shadowY;
     this.width = 350;
     this.height = 435;
-    this.vx = 2;
-    this.vy = 2;
+    this.vx = 0;
+    this.vy = 0;
     this.maxSpeed = 4;
-    this.outlineWidth = 375;
-    this.outlineHeight = 460;
-    this.handleWidth = 280;
-    this.handleHeight = 140;
-    this.shadowWidth = 180;
-    this.shadowHeight = 70;
-    this.altitude = -2;
-    this.start = 0;
-    this.stop = PI
-  }
-
-  float(){
-    let change = random(0, 1);
-    if (change < 0.1) {
-      this.altitude = random(-this.maxSpeed, this.maxSpeed);
-    }
-    this.shadowY = this.shadowY + this.altitude;
-}
-
-  tremble(){
-    let r = random(0, 1);
-    if (r < 0.1) {
-      this.vx = random(-this.maxSpeed, this.maxSpeed);
-      this.vy = random(-this.maxSpeed, this.maxSpeed);
-    }
+    this.broken = true;
+    this.start = 0;  // needed to draw Arc (line 42)
+    this.stop = PI   // needed to draw Arc (line 42)
   }
 
   move(){
-    // Move the ghost
-    this.x += this.vx;
-    this.y += this.vy;
+
+    this.x =  this.x + this.vx;
+    this.y =  this.y + this.vy;
+
+  }
+
+  tremble(){
+
+    // Lights gently floating around screen
+    let change = random(0, 1);
+    if (change < 5){
+      this.vx = random(-this.maxSpeed, this.maxSpeed);
+    }
+    this.x = constrain( this.x, 16*width/33, 17*width/33);
+
   }
 
 
-
   display(){
-    // Mirror "Handle"
+    // "Handle"
     push();
     noFill();
     stroke(255);
     strokeWeight(5);
-    arc(this.x, this.handleY, this.handleWidth, this.handleHeight, this.start, this.stop);
+    arc(this.x, this.y + this.height/3, 5*this.width/7, this.height/3, this.start, this.stop);
     pop();
 
     // Mirror
@@ -61,26 +47,28 @@ class Mirror{
     strokeWeight(9);
     ellipse(this.x, this.y - this.y/5, this.width, this.height);
     strokeWeight(2);
-    ellipse(this.x, this.y - this.y/5, this.outlineWidth, this.outlineHeight);
+    ellipse(this.x, this.y - this.y/5, 15*this.width/14, 92*this.height/87);
     pop();
 
-    // Mirror "Shadow" (Negative)
+    // "Shadow" (Negative)
     push();
     noStroke();
-    fill(255, 255, 255, 190);
-    ellipse(this.shadowX, this.shadowY, this.shadowWidth, this.shadowHeight);
+    let r = random(70, 220);
+    fill(255, 255, 255, r);
+    ellipse(this.x, this.y + 3*this.height/4, 3*this.width/7, this.height/7);
     pop();
 
     // Cracks
-    push();
-    stroke(255);
-    strokeWeight(2);
-    line(this.x + 13*this.width/35, this.y - 46*this.height/87, this.x - 3*this.width/7, this.y + 2*this.height/29);
-    line(this.x - this.width/4, this.y - 56*this.height/87, this.x + this.width/7, this.y - 32*this.height/87);
-    line(this.x + this.width/15, this.y - 26*this.height/87, this.x + this.width/4, this.y + 19*this.height/87);
-    line(this.x + this.width/7, this.y - this.height/8, this.x + this.width/2, this.y - this.height/4);
-    pop();
-
+    if(this.broken){
+      push();
+      stroke(255);
+      strokeWeight(2);
+      line(this.x + 13*this.width/35, this.y - 46*this.height/87, this.x - 3*this.width/7, this.y + 2*this.height/29);
+      line(this.x - this.width/4, this.y - 56*this.height/87, this.x + this.width/7, this.y - 32*this.height/87);
+      line(this.x + this.width/15, this.y - 26*this.height/87, this.x + this.width/4, this.y + 19*this.height/87);
+      line(this.x + this.width/7, this.y - this.height/8, this.x + this.width/2, this.y - this.height/4);
+      pop();
+    }
   }
 
 
