8d986622ab3de32bda7b77205d64f03697fba820
Angel CC <angelcenerini@gmail.com>
2020-12-06 23:12:07 -0500

    P02DTT: Created/Inserted in main script TipsTable Class
    To be adjusted

---
+++ Projects/Project02/Drafts/TipsTable/js/TipsTable.js
@@ -0,0 +1,71 @@
+class TipsTable{
+  constructor(x, y, positionX, positionY, tips){
+  this.x = x;
+  this.y = y;
+  this.positionX = positionX;
+  this.positionY = positionY;
+  this.size = 600;
+  this.active = false;
+  this.keyCode = undefined;   // SPACEBAR
+  this.tips = cues;
+   // Guessing Answer
+   this.currentIndex = 0;
+   this.answer = `stop`;    // Word User has to type in order to surpass level
+   this.currentInput = ``;  // Keeping Track of User's Input
+   this.state = undefined;
+   }
+
+   keyPressed(){
+     // Tips Table appearing/disappearing when User presses SPACEBAR
+     if (this.keyCode === 32){
+     if(tipsTable.active === false){
+        tipsTable.active = true;
+     }
+     else {
+        tipsTable.active = false;
+     }
+    }
+
+    else if (this.keyCode === 8 ) { //&& state === `level`
+    // User Resets Inserted Input
+    this.currentInput = ``;
+  }
+   }
+
+   mousePressed(){
+   // if (state === `level`){
+   // Display next Cue
+     this.currentIndex = this.currentIndex + 1;
+     if (this.currentIndex === this.tips.length){
+         this.currentIndex = 0;
+     }
+   }
+
+   keyTyped() {
+     // Type Input
+     if (this.keyCode !== 13){  // && state === `level`
+     this.currentInput += key;
+     }
+   }
+
+  display(){
+   // Tips Table to guess mystery word
+   if(!this.active){
+     return;
+   }
+
+   push();
+   // Display Semi-Transparent Tips Table
+   fill(255, 255, 255, 150);
+   rect(this.x, this.y, this.size);
+   // Display Tips Table White Text
+   fill(0);
+   textAlign(CENTER, CENTER);
+   textSize(20);
+   text(this.tips, this.x, this.y);
+   fill(255);
+   textSize(15);
+   text(`Click for more tips >>`,this.positionX, this.positionY);
+   pop();
+}
+}
--- Projects/Project02/Drafts/TipsTable/js/script.js
+++ Projects/Project02/Drafts/TipsTable/js/script.js
@@ -4,12 +4,31 @@ Angel Cella Cenerini
 
 Project 02 - Drafts: TipsTable OOP
 **************************************************/
-
+let tipsTable;
+let cues = [
+  ``,
+  `1. What a lovely keyboard you have there,
+  lying on your desk.`,
+  `2. I am flickering.`,
+  `3. Don't read me like that.`,
+  `4. Try me backwards.`,
+  `5. Did you get it yet? Type me!`
+];
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
 createCanvas(windowWidth, windowHeight);
+noStroke();
+rectMode(CENTER);
+
+let x = width/2;
+let y = height/2;
+let positionX = 2*width/5;
+let positionY = 4*height/5;
+let tips = cues;
+tipsTable = new TipsTable(x, y, positionX, positionY, tips);
+
 }
 
 // draw()
@@ -17,4 +36,19 @@ createCanvas(windowWidth, windowHeight);
 // Description of draw() goes here.
 function draw() {
 background(0);
+
+
+tipsTable.display();
+}
+
+function keyPressed(){
+  tipsTable.keyPressed();
+}
+
+function keyTyped(){
+  tipsTable.keyTyped();
+}
+
+function mousePressed(){
+  tipsTable.mousePressed();
 }
