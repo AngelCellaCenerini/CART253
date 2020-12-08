ec3d9a3bf5b017344fd78eb150a6ed2b0c79b2d4
Angel CC <angelcenerini@gmail.com>
2020-12-07 21:07:22 -0500

    P02: Basic SetUp
    - Provided Title/Description;
    - setup()/draw(): basic settings;

--- Projects/Project02/FinalProject02/js/script.js
+++ Projects/Project02/FinalProject02/js/script.js
@@ -2,16 +2,30 @@
 "use strict";
 
 /**************************************************
-Template p5 project
-Pippin Barr
+'Madeleine' - Project 02: Anything
+Angel Cella Cenerini
+Template p5 by CART253 Course
 
-Here is a description of this template p5 project.
+
+- MADELEINE -
+
+A pictorial journey throughout one's unconsciousness.
+
+The program develops into five levels, each guarding hidden enigmas. The User can either
+survive the challenge or solve the puzzle and surpass the level, thus achieving a collectable item (either a Voice or a Script Shred).
+Depending on how well the User performs, in the end they will be able to play a melody, concluding this experience.
+
+Further technical and conceptual details are provided in the README.md file.
 **************************************************/
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(windowWidth, windowHeight);
+  noStroke();
+  rectMode(CENTER);
+  userStartAudio();
 
 }
 
@@ -19,5 +33,7 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
+
+  background(0); // black
 
 }
